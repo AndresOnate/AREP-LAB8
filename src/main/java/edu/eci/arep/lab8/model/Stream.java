2c582fd55b113f920d69d65eb84d67d57b77a562
Mateo0laya <mateoolaya20032604@gmail.com>
2024-03-31 18:28:18 -0500

    Iterator with stack

--- src/main/java/edu/eci/arep/lab8/model/Stream.java
+++ src/main/java/edu/eci/arep/lab8/model/Stream.java
@@ -1,14 +1,25 @@
 package edu.eci.arep.lab8.model;
 
-import java.util.List;
+import java.util.Iterator;
+import java.util.Stack;
 
 public class Stream {
 
-    private List<Post> stream;
+    private Stack<Post> stream;
+
+    public Stream() {
+        stream = new Stack<>();
+    }
+
+    public void addPost(Post post) {
+        stream.push(post);
+    }
 
     public String toString() {
         String res = "";
-        for (Post post : stream) {
+        Iterator<Post> iterator = stream.iterator();
+        while (iterator.hasNext()) {
+            Post post = iterator.next();
             res += post.getOwner().getUsername() + "\n " + post.getContent() + "\n";
         }
         return res;
