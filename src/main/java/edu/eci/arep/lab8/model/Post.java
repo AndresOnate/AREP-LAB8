a18b43c59a0553b8a0769a0d9411598813e3b46a
Mateo0laya <mateoolaya20032604@gmail.com>
2024-03-31 18:13:37 -0500

    Model full and main class

--- src/main/java/edu/eci/arep/lab8/model/Post.java
+++ src/main/java/edu/eci/arep/lab8/model/Post.java
@@ -2,4 +2,20 @@ package edu.eci.arep.lab8.model;
 
 public class Post {
 
+    private User owner;
+    private String content;
+
+    public Post(User owner, String content) {
+        this.owner = owner;
+        this.content = content;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
 }
--- src/main/java/edu/eci/arep/lab8/model/Stream.java
+++ src/main/java/edu/eci/arep/lab8/model/Stream.java
@@ -1,5 +1,16 @@
 package edu.eci.arep.lab8.model;
 
+import java.util.List;
+
 public class Stream {
 
+    private List<Post> stream;
+
+    public String toString() {
+        String res = "";
+        for (Post post : stream) {
+            res += post.getOwner().getUsername() + "\n " + post.getContent() + "\n";
+        }
+        return res;
+    }
 }
