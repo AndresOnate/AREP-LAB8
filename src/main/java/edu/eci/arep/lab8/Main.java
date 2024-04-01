2f5d98624b36b0b4d742f90e5ce7c44c0f3eec3b
nicolas.ariza241@gmail.com <nicolas.ariza241@gmail.com>
2024-03-31 19:25:45 -0500

    Feature terminado

--- src/main/java/edu/eci/arep/lab8/Main.java
+++ src/main/java/edu/eci/arep/lab8/Main.java
@@ -1,19 +1,15 @@
 package edu.eci.arep.lab8;
 
 import jakarta.annotation.security.PermitAll;
-import jakarta.annotation.security.RolesAllowed;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.InternalServerErrorException;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.SecurityContext;
 
-import org.eclipse.microprofile.jwt.Claim;
-import org.eclipse.microprofile.jwt.Claims;
 import org.eclipse.microprofile.jwt.JsonWebToken;
 
 @Path("/notTwitter")
--- src/main/java/edu/eci/arep/lab8/model/Stream.java
+++ src/main/java/edu/eci/arep/lab8/model/Stream.java
@@ -15,13 +15,19 @@ public class Stream {
         stream.push(post);
     }
 
-    public String toString() {
-        String res = "";
+    public String toJSON() {
+        String res = "{";
         Iterator<Post> iterator = stream.iterator();
-        while (iterator.hasNext()) {
+        // Se extraen los posts y se agregan a una coleccion JSON
+        while (iterator.hasNext()) { 
             Post post = iterator.next();
-            res += post.getOwner().getUsername() + "\n " + post.getContent() + "\n";
+            res += "{\"owner\":\"" + post.getOwner().getUsername() + "\"," +
+                        "\"content\":\"" + post.getContent() + "\"},";
         }
+        // Se elimina la utlima coma restante --> ...},
+        res = res.substring(0, res.length() - 1);
+        // Se cierra el JSON
+        res += "}";
         return res;
     }
 }
