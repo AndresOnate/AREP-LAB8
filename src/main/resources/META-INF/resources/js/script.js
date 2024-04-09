01b6acb838cfca249f15e56ac3fcba027bd43625
nicolas.ariza241@gmail.com <nicolas.ariza241@gmail.com>
2024-04-09 17:41:29 -0500

    Enchanced for cloud da js

--- src/main/resources/META-INF/resources/js/script.js
+++ src/main/resources/META-INF/resources/js/script.js
@@ -1,63 +1,69 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // getPosts();
+    getPosts();
     validateToken();
 
-    // document.getElementById('post-button').addEventListener('click', function() {
-    //     const postText = document.getElementById('post-text').value;
-    //     if (postText.trim() !== ''  && postText.length <= 140) {
+    document.getElementById('post-button').addEventListener('click', function() {
+        const postText = document.getElementById('post-text').value;
+        if (postText.trim() !== ''  && postText.length <= 140) {
 
-    //         const tempUser = {
-    //             username: 'usuario_temporal',
-    //             password: 'contraseña_temporal'
-    //         };
-
-    //         const postData = {
-    //             owner: tempUser,
-    //             content: postText
-    //         };
-    //         fetch('/posts', {
-    //             method: 'POST',
-    //             headers: {
-    //                 'Content-Type': 'application/json'
-    //             },
-    //             body: JSON.stringify(postData)
-    //         })
-    //         .then(response => {
-    //             if (response.ok) {
-    //                 document.getElementById('post-text').value = '';
-    //                 getPosts();
-    //             } else {
-    //                 console.error('Error al enviar el post:', response.statusText);
-    //             }
-    //         })
-    //         .catch(error => console.error('Error al enviar el post:', error));
-    //     } else {
-    //         console.error('El campo de texto está vacío');
-    //     }
-    // });
+            const postData = {
+                owner: getUserEmail(),
+                content: postText
+            };
+			
+            fetch('https://kbbu4x3jxa.execute-api.us-east-1.amazonaws.com/dev/posts', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+					'Authorization' : localStorage.getItem('ACCESS-TOKEN')
+                },
+                body: JSON.stringify(postData)
+            })
+            .then(response => {
+                if (response.ok) {
+                    document.getElementById('post-text').value = '';
+                    getPosts();
+                } else {
+                    console.error('Error al enviar el post:', response.statusText);
+                }
+            })
+            .catch(error => console.error('Error al enviar el post:', error));
+        } else {
+            console.error('El campo de texto está vacío');
+        }
+    });
 });
 
 
-// function getPosts() {
-//     fetch('/streams')
-//         .then(response => response.json())
-//         .then(posts => {
-//             document.getElementById('post-list').innerHTML = '';
-//             posts.reverse().forEach(post => {
-//                 addPostToPage(post);
-//             });
-//         })
-//         .catch(error => console.error('Error al obtener los posts:', error));
-// }
+function getPosts() {
+    fetch('https://kbbu4x3jxa.execute-api.us-east-1.amazonaws.com/dev/streams', {
+		method: 'GET',
+		headers: 'Authorization' : localStorage.getItem('ACCESS-TOKEN')
+	})
+        .then(response => response.json())
+        .then(posts => {
+            document.getElementById('post-list').innerHTML = '';
+            posts.reverse().forEach(post => {
+                addPostToPage(post);
+            });
+        })
+        .catch(error => console.error('Error al obtener los posts:', error));
+}
 
-// function addPostToPage(post) {
-//     const postDiv = document.createElement('div');
-//     postDiv.className = 'post';
-//     const postText = document.createElement('p');
-//     postText.textContent = post.content; 
-//     postDiv.appendChild(postText);
-//     document.getElementById('post-list').appendChild(postDiv);
-// }
+function addposttopage(post) {
+    const postdiv = document.createelement('div');
+    postdiv.classname = 'post';
+	
+	
+    const postOwner = document.createelement('p');
+    postOwner.textcontent = 'Autor : ' + getUserEmail();
+    postdiv.appendchild(postOwner);
+
+	const postText = document.createelement('p');
+    postText.textcontent = 'Autor: ' + post.content;
+    postdiv.appendchild(postText);
+    document.getelementbyid('post-list').appendchild(postdiv);
+}
 
 function validateToken(){
     var accessToken = localStorage.getItem('ACCESS-TOKEN');
@@ -73,5 +79,11 @@ function validateToken(){
 
 function getAccessTokenFromUrl() {
     var fragment = window.location.hash.substring(1);
-    return fragment.split('=')[1];
+    return fragment.split('&')[0].split('=')[1];
+}
+
+function getUserEmail(){
+	const jwtToken = localStorage.getItem('ACCESS-TOKEN');
+	const decodedToken = JSON.parse(atob(jwtToken.split('.')[1]));
+	return decodedToken.email;
 }
